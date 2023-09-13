ae20511ba00dada90e6c88df2457e36b5b44d1c0
TrAyZeN <1810leo@gmail.com>
2023-09-13 15:47:01 +0200

    Upgrade dependencies

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React, { setGlobal, addReducer } from 'reactn';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.scss';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
@@ -43,7 +43,8 @@ addReducer("removeFrame", (global, dispatch) => {
     }
 });
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
+root.render(<App />, );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
